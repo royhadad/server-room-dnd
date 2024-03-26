346f051d2430f06ff5d360a2b6a994f92131f340
Roy Hadad <roy@datree.io>
2024-03-26 23:38:06 +0800

    refactor: add emotion css, improve eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,11 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
@@ -14,5 +17,15 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+    "react/no-unknown-property": ["error", { ignore: ["css"] }],
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: { version: "detect" },
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import serverRackIcon from "./assets/server-rack-icon.svg";
 import "./App.css";
+import { css } from "@emotion/react";
 
 function App() {
   return (
     <div>
-      <h1>hello world!</h1>
+      <h1
+        css={css`
+          font-size: 5rem;
+        `}
+      >
+        hello world!
+      </h1>
       <div>
-        <img src={serverRackIcon} className="logo" alt="Vite logo" />
+        <img
+          src={serverRackIcon}
+          alt="Server Rack"
+          css={css`
+            width: 100px;
+            height: 100px;
+          `}
+        />
       </div>
     </div>
   );
